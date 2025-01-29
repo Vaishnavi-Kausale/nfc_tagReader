fb2d55ba589619284862afcb79d0f45663b82403
Vaishnavi-Kausale <vaishnavikausale@gmail.com>
2025-01-29 15:21:16 +0530

    updated nfc tag scan and then opening app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 import 'package:nfc_tagreader/presentation/screen/read_write_nfc_screen.dart';
 
 void main() {
@@ -16,7 +17,39 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ReadWriteNFCScreen()
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("NFC Tag Reader")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "App opened via NFC scan!",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ReadWriteNFCScreen()),
+                );
+              },
+              child: const Text("Scan NFC Tag"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
